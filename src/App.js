535fd5164f09391af2193a9ff3ab2807d6798048
Kush21singh <ankush76728@gmail.com>
2023-10-17 20:49:15 +0530

    Ecom

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import MusicAlbumShoppingPage from './MusicAlbumShoppingPage';
+import Footer from './Footer';
+import'./App.css';
+function App() {
+ return (
+    <div className="App">
+      <Header />
+      <MusicAlbumShoppingPage />
+      <Footer />
+    </div>
+ );
+}
+
+export default App;
